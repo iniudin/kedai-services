375ea958f92eba43fbfcc64ff2e832e870e3d378
iniudin <sysfdn@pm.me>
2026-05-06 16:44:53 +0700

    refactor: using prefix at router controller

--- src/controller/add-ons-controller.ts
+++ src/controller/add-ons-controller.ts
@@ -4,80 +4,60 @@ import * as commonModel from '@/model/common-model'
 import { ErrorResponse } from '@/model/common-model'
 import * as addOnsService from '@/service/add-ons-service'
 
-export const addOnsController = new Elysia()
-  .post(
-    '/add-ons',
-    async ({ body }) => {
-      const result = await addOnsService.createAddOn(body)
-      return result
+export const addOnsController = new Elysia({ prefix: '/add-ons' })
+  .post('', async ({ body }) => {
+    const result = await addOnsService.createAddOn(body)
+    return result
+  }, {
+    body: addOnsModel.createAddOnSchema,
+    response: {
+      201: addOnsModel.addOnSchema,
+      500: commonModel.ErrorResponse,
     },
-    {
-      body: addOnsModel.createAddOnSchema,
-      response: {
-        201: addOnsModel.addOnSchema,
-        500: commonModel.ErrorResponse,
-      },
+  })
+  .get('', async () => {
+    const result = await addOnsService.listAddOns()
+    return result
+  }, {
+    response: {
+      200: addOnsModel.listAddOnResponseSchema,
+      500: ErrorResponse,
     },
-  )
-  .get(
-    '/add-ons',
-    async () => {
-      const result = await addOnsService.listAddOns()
-      return result
+  })
+  .get('/:id', async ({ params: { id } }) => {
+    const result = await addOnsService.findAddOnById(id)
+    return result
+  }, {
+    params: t.Object({
+      id: t.Numeric(),
+    }),
+    response: {
+      200: addOnsModel.addOnSchema,
+      500: commonModel.ErrorResponse,
     },
-    {
-      response: {
-        200: addOnsModel.listAddOnResponseSchema,
-        500: ErrorResponse,
-      },
+  })
+  .put('/:id', async ({ params: { id }, body }) => {
+    const result = await addOnsService.updateAddOn(id, body)
+    return result
+  }, {
+    params: t.Object({
+      id: t.Numeric(),
+    }),
+    body: addOnsModel.updateAddOnSchema,
+    response: {
+      200: addOnsModel.addOnSchema,
+      500: commonModel.ErrorResponse,
     },
-  )
-  .get(
-    '/add-ons/:id',
-    async ({ params: { id } }) => {
-      const result = await addOnsService.findAddOnById(id)
-      return result
+  })
+  .delete('/:id', async ({ params: { id } }) => {
+    const result = await addOnsService.deleteAddOn(id)
+    return result
+  }, {
+    params: t.Object({
+      id: t.Numeric(),
+    }),
+    response: {
+      200: addOnsModel.addOnSchema,
+      500: commonModel.ErrorResponse,
     },
-    {
-      params: t.Object({
-        id: t.Numeric(),
-      }),
-      response: {
-        200: addOnsModel.addOnSchema,
-        500: commonModel.ErrorResponse,
-      },
-    },
-  )
-  .put(
-    '/add-ons/:id',
-    async ({ params: { id }, body }) => {
-      const result = await addOnsService.updateAddOn(id, body)
-      return result
-    },
-    {
-      params: t.Object({
-        id: t.Numeric(),
-      }),
-      body: addOnsModel.updateAddOnSchema,
-      response: {
-        200: addOnsModel.addOnSchema,
-        500: commonModel.ErrorResponse,
-      },
-    },
-  )
-  .delete(
-    '/add-ons/:id',
-    async ({ params: { id } }) => {
-      const result = await addOnsService.deleteAddOn(id)
-      return result
-    },
-    {
-      params: t.Object({
-        id: t.Numeric(),
-      }),
-      response: {
-        200: addOnsModel.addOnSchema,
-        500: commonModel.ErrorResponse,
-      },
-    },
-  )
+  })
--- src/controller/products-controller.ts
+++ src/controller/products-controller.ts
@@ -4,80 +4,60 @@ import { ErrorResponse } from '@/model/common-model'
 import * as productModel from '@/model/products-model'
 import * as productsService from '@/service/products-service'
 
-export const productsController = new Elysia()
-  .post(
-    '/products',
-    async ({ body }) => {
-      const result = await productsService.createProduct(body)
-      return result
+export const productsController = new Elysia({ prefix: '/products' })
+  .post('', async ({ body }) => {
+    const result = await productsService.createProduct(body)
+    return result
+  }, {
+    body: productModel.createProductSchema,
+    response: {
+      201: productModel.productSchema,
+      500: commonModel.ErrorResponse,
     },
-    {
-      body: productModel.createProductSchema,
-      response: {
-        201: productModel.productSchema,
-        500: commonModel.ErrorResponse,
-      },
+  })
+  .get('', async () => {
+    const result = await productsService.listProducts()
+    return result
+  }, {
+    response: {
+      200: productModel.listProductResponseSchema,
+      500: ErrorResponse,
     },
-  )
-  .get(
-    '/products',
-    async () => {
-      const result = await productsService.listProducts()
-      return result
+  })
+  .get(':id', async ({ params: { id } }) => {
+    const result = await productsService.findProductById(id)
+    return result
+  }, {
+    params: t.Object({
+      id: t.Numeric(),
+    }),
+    response: {
+      200: productModel.productSchema,
+      500: commonModel.ErrorResponse,
     },
-    {
-      response: {
-        200: productModel.listProductResponseSchema,
-        500: ErrorResponse,
-      },
+  })
+  .put(':id', async ({ params: { id }, body }) => {
+    const result = await productsService.updateProduct(id, body)
+    return result
+  }, {
+    params: t.Object({
+      id: t.Numeric(),
+    }),
+    body: productModel.updateProductSchema,
+    response: {
+      200: productModel.productSchema,
+      500: commonModel.ErrorResponse,
     },
-  )
-  .get(
-    '/products/:id',
-    async ({ params: { id } }) => {
-      const result = await productsService.findProductById(id)
-      return result
+  })
+  .delete(':id', async ({ params: { id } }) => {
+    const result = await productsService.deleteProduct(id)
+    return result
+  }, {
+    params: t.Object({
+      id: t.Numeric(),
+    }),
+    response: {
+      200: productModel.productSchema,
+      500: commonModel.ErrorResponse,
     },
-    {
-      params: t.Object({
-        id: t.Numeric(),
-      }),
-      response: {
-        200: productModel.productSchema,
-        500: commonModel.ErrorResponse,
-      },
-    },
-  )
-  .put(
-    '/products/:id',
-    async ({ params: { id }, body }) => {
-      const result = await productsService.updateProduct(id, body)
-      return result
-    },
-    {
-      params: t.Object({
-        id: t.Numeric(),
-      }),
-      body: productModel.updateProductSchema,
-      response: {
-        200: productModel.productSchema,
-        500: commonModel.ErrorResponse,
-      },
-    },
-  )
-  .delete(
-    '/products/:id',
-    async ({ params: { id } }) => {
-      const result = await productsService.deleteProduct(id)
-      return result
-    },
-    {
-      params: t.Object({
-        id: t.Numeric(),
-      }),
-      response: {
-        200: productModel.productSchema,
-        500: commonModel.ErrorResponse,
-      },
-    },
-  )
+  })
